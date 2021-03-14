b2a1c1e9d074a97f82a101b46e0fb2b2b9cdc25e
Arif <arifento85@gmail.com>
2021-03-14 19:24:08 +0500

    release 3

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import { enableES5 } from 'immer';
+import { AppRegistry } from 'react-native';
 
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
+enableES5();
 AppRegistry.registerComponent(appName, () => App);
